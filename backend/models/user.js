fe0c2610c663fafc8468876b1a62a61c9df3a59d
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-06-28 13:33:47 +0530

    user model updation

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,10 +1,29 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["learner", "educator", "coordinator"], default: "learner" },
+  name: { 
+    type: String, 
+    required: true 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  role: { 
+    type: String, 
+    enum: ["learner", "educator"], 
+    default: "learner" 
+  },
+  status: {
+    type: String,
+    enum: ["pending", "approved", "rejected"],
+    default: "pending"
+  },
   designation: { type: String },
   institution: { type: String },
   institution_id: { type: String },
