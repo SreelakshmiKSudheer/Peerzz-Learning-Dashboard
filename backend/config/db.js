21416bfe7f6d659e4f0956e099827e40ce27f8aa
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-06-28 17:33:33 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-console.log(process.env.MONGODB_URI);
 // Connect to MongoDB
 async function connectDB() {
   try {
