cf0f61b3038bb6663b7a372aad551c9a9b780d5e
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-07-05 11:55:19 +0530

    user logout

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const { register, login } = require('../controllers/authController');
+const { registerByRole, login, logout } = require('../controllers/authController');
 
 const authRouter = express.Router();
 
 // // http://localhost:3000/api/auth/register
-authRouter.post('/register', register);
+authRouter.post('/register/:role', registerByRole);
 // // http://localhost:3000/api/auth/login
 authRouter.post('/login', login);
+
+// // http://localhost:3000/api/auth/logout
+authRouter.post('/logout', logout);
 
 
 module.exports = authRouter;
