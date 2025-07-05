50ded33020b3b31f4056a28176d94bbeb1a53d65
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-07-05 11:55:35 +0530

    status update

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 
+// The profile_url field is intended to store the URL of the user's profile picture or page.
+// Whether it is provided by the platform or the user depends on your application logic.
+// If you want the platform to provide a default profile URL, you can set a default value here.
+
 const userSchema = new mongoose.Schema({
   name: { 
     type: String, 
@@ -21,15 +25,14 @@ const userSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    enum: ["pending", "approved", "rejected"],
-    default: "pending"
+    enum: ["registered", "approved", "rejected"],
+    default: "registered"
   },
   designation: { type: String },
   institution: { 
     type: mongoose.Schema.Types.ObjectId, 
     ref: "Institution" 
   },
-  profile_url: { type: String },
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
