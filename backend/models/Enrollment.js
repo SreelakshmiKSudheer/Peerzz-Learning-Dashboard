49d0aa8895f7b276273d29f8cd10b35fe0a39235
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-07-06 16:46:49 +0530

    enrollment schema

---
+++ backend/models/Enrollment.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const User = require("./User");
+const Course = require("./Course");
+
+const enrollmentSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  course: { type: mongoose.Schema.Types.ObjectId, ref: "Course" },
+  dateEnrolled: { type: Date, default: Date.now },
+    status: {
+        type: String,
+        enum: ["enrolled", "completed", "dropped"],
+        default: "enrolled",
+    },
+});
+
+module.exports = mongoose.model("Enrollment", enrollmentSchema);
