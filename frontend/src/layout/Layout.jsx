8edf4700bc9f96ad39c4effd4e0781d9e1b19c4f
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-07-25 09:31:59 +0530

    layout update

---
+++ frontend/src/layout/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import { Outlet } from 'react-router'
+
+const Layout = () => {
+    return (
+        <div className='bg-[var(--uni)]'>
+            <Header /> 
+            <main className="min-h-screen px-4 py-6 text-[var(--tri)]">
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
