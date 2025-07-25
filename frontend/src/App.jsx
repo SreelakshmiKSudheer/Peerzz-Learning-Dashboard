04059fc2f0af90a2b3f12363832bd51cc0f93978
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-07-25 09:31:12 +0530

    color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import Layout from './layout/Layout';
+import PublicHome from './pages/PublicHome';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import AdminDashboard from './pages/AdminDashboard';
+import EducatorDashboard from './pages/EducatorDashboard';
+import LearnerDashboard from './pages/LearnerDashboard';
+import EducatorSignUp from './pages/EducatorSignUp';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<PublicHome />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/educator-dashboard" element={<EducatorDashboard />} />
+        <Route path="/learner-dashboard" element={<LearnerDashboard />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<SignUp />} />
+      <Route path="/register-teacher" element={<EducatorSignUp />} />
+    </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
