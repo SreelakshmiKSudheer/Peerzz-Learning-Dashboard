c1665f00e9e677f5d9c9d1a8f51f0c959773fab1
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-09-01 14:04:42 +0530

    path linkings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,21 +7,30 @@ import AdminDashboard from './pages/AdminDashboard';
 import EducatorDashboard from './pages/EducatorDashboard';
 import LearnerDashboard from './pages/LearnerDashboard';
 import EducatorSignUp from './pages/EducatorSignUp';
+import CourseCreation from './pages/CourseCreation';
+import Profile from './pages/Profile';
+import Users from './pages/Users';
+import SingleUser from './pages/SingleUser';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<PublicHome />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/educator-dashboard" element={<EducatorDashboard />} />
-        <Route path="/learner-dashboard" element={<LearnerDashboard />} />
-      </Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<SignUp />} />
-      <Route path="/register-teacher" element={<EducatorSignUp />} />
-    </Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<PublicHome />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/educator-dashboard" element={<EducatorDashboard />} />
+          <Route path="/learner-dashboard" element={<LearnerDashboard />} />
+          <Route path="/course/create" element={<CourseCreation />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/admin/users/user/:userId" element={<SingleUser />} />      
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route path="/register-teacher" element={<EducatorSignUp />} />
+      </Routes>
     </Router>
   );
 }
