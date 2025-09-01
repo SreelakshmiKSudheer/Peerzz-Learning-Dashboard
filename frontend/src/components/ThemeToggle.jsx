d76d0191d53ea0157c156e023190085026a69197
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-09-01 14:04:21 +0530

    theme toggle

--- frontend/src/components/ThemeToggle.jsx
+++ frontend/src/components/ThemeToggle.jsx
@@ -1,9 +1,7 @@
 // ThemeToggle.js
 import { useEffect, useState } from 'react';
-<<<<<<< HEAD
 import { SunMedium, Moon } from 'lucide-react'
-=======
->>>>>>> e3def797fc9c3fde80c488c5233ca0597f5fbfa4
+
 
 export default function ThemeToggle() {
   const [darkMode, setDarkMode] = useState(() =>
@@ -23,7 +21,6 @@ export default function ThemeToggle() {
 
   return (
     <button
-<<<<<<< HEAD
       className="p-2 rounded border-2 border-[var(--tri)]"
       onClick={() => setDarkMode(prev => !prev)}
       aria-label="Toggle Theme"
@@ -34,14 +31,3 @@ export default function ThemeToggle() {
   );
 }
 
-=======
-      className="px-4 py-2 bg-[var(--quad)] text-[var(--tri)] rounded"
-      onClick={() => setDarkMode(prev => !prev)}
-    >
-      Toggle Theme
-    </button>
-  );
-}
-
-// [var(--color-quaternary)]
->>>>>>> e3def797fc9c3fde80c488c5233ca0597f5fbfa4
