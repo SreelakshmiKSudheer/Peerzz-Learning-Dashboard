057a9b8a573dd77e5f9a995dcd818bcefe17df9f
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-09-01 13:15:07 +0530

    db config console added

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,7 @@ dotenv.config();
 async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
+    console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
     process.exit(1); // Exit the process with failure
